d84a687133bb982f98f2fd1bd8ddab7bddd4129d
zacharyasmith <zach@medialusions.com>
2017-08-31 11:28:42 -0600

    Install semantic-ui

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>PCB Automation</title>
+  <link rel="stylesheet" href="src/semantic/dist/semantic.min.css">
+</head>
+<body>
+  <div class="ui fixed inverted menu">
+    <div class="ui container">
+      <a href="#" class="header item">PCB Automation</a>
+      <a href="#" class="item">Home</a>
+      <a href="#" class="item">Team</a>
+      <a href="#" class="item">Timeline</a>
+      <a href="#" class="item">Pictures</a>
+      <a href="#" class="item">Documents</a>
+      <a href="#" class="item">Acknowledgements</a>
+    </div>
+  </div>
+
+  <div class="_header_clear"></div>
+  
+  <div class="ui container">
+    <h1 class="ui header">Welcome!</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+  </div>
+
+  <div class="ui bottom fixed inverted compact aligned menu">
+          <a class="item" target="_blank" href="https://www.colostate.edu/equal-opportunity/">Equal Opportunity</a>
+          <a class="item" target="_blank" href="https://www.colostate.edu/disclaimer/">Disclaimer</a>
+          <a class="item" target="_blank" href="https://www.colostate.edu/privacy/">Privacy Policy</a>
+          <a class="item" target="_blank" href="https://www.colostate.edu/search">Search CSU</a>
+          <a class="item" target="_blank" href="https://www.colostate.edu/contact/">Contact CSU</a>
+  </div>
+</body>
+</html>
