d3665afaf8d942e626bc2ab97d0f5e24b8d418b8
zacharyasmith <zach@medialusions.com>
2017-08-31 11:44:10 -0600

    Boilerplate for rest of site

---
+++ sections/footer.php
@@ -0,0 +1,9 @@
+<div class="ui bottom fixed inverted compact aligned menu">
+        <a class="item" target="_blank" href="https://www.colostate.edu/equal-opportunity/">Equal Opportunity</a>
+        <a class="item" target="_blank" href="https://www.colostate.edu/disclaimer/">Disclaimer</a>
+        <a class="item" target="_blank" href="https://www.colostate.edu/privacy/">Privacy Policy</a>
+        <a class="item" target="_blank" href="https://www.colostate.edu/search">Search CSU</a>
+        <a class="item" target="_blank" href="https://www.colostate.edu/contact/">Contact CSU</a>
+</div>
+</body>
+</html>
---
+++ sections/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <!-- Standard Meta -->
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+
+  <title>PCB Automation</title>
+  <link rel="stylesheet" href="src/semantic/dist/semantic.min.css">
+  <link rel="stylesheet" href="src/style.css">
+</head>
+<body>
+  <div class="ui fixed inverted menu">
+    <div class="ui container">
+      <a href="#" class="item"><h2>PCB Automation</h2></a>
+      <div class="right menu">
+        <a href="#" class="item">Home</a>
+        <a href="#" class="item">Team</a>
+        <a href="#" class="item">Timeline</a>
+        <a href="#" class="item">Pictures</a>
+        <a href="#" class="item">Documents</a>
+        <a href="#" class="item">Acknowledgements</a>
+      </div>
+    </div>
+  </div>
+
+  <div class="_header_clear"></div>
