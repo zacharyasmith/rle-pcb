669f1f55e39d5446aee1b55921f6dffce0d98374
zacharyasmith <zach@medialusions.com>
2017-09-22 14:06:51 -0600

    Updated team page with recent info.

--- sections/team.php
+++ sections/team.php
@@ -4,27 +4,64 @@
     <h1 class="ui inverted dividing header">Meet the Team</h1>
     <div class="ui three cards">
       <div class="card">
+        <div class="image">
+          <img src="img/team/jessica_boyd.jpg">
+        </div>
         <div class="content">
           <div class="header">Jessica Boyd</div>
           <div class="meta">
             Electrical Engineer Undergrad
           </div>
+          <div class="description">
+            I have had four internships with three different companies. I interned with EG Power Engineering, right out of high school and then again for my freshman summer, where I used AutoCad to draft architectural layouts of power distribution systems. In my sophomore summer, I interned with HP inc. on the Laser Jet Hardware Enterprise Solutions team (Laser Jet Printers). At HP, I helped conceptualize and create block diagrams, schematics, layouts and the material lists of two circuits. One circuit was a serial communication translator. The second was a temperature sensor. For my junior summer, I interned for Keysight Technologies working with their Technology Leadership Organization and Keysight Labs on embedded operating systems.
+          </div>
+        </div>
+        <div class="extra content">
+          <a href=“https://www.linkedin.com/in/jessicaboydelectricalengineer/“ target=“_blank”>
+            <i class="linkedin square icon"></i>
+            Profile
+          </a>
         </div>
       </div>
       <div class="card">
+        <div class="image">
+          <img src="img/team/jeremy_deutch.png">
+        </div>
         <div class="content">
           <div class="header">Jeremy Deutch</div>
           <div class="meta">
             Mechanical Engineer Undergrad
           </div>
+          <div class="description">
+            <p>I’ve been around the block a few more times than most undergraduates as I begin my seventh and final year at CSU! Over those seven years, I have had internships / co-ops with 3 different companies (1.5 additional years), changed my major twice (1 additional year) and held innumerable part time jobs.</p>
+            <p>In addition to my professional experience, I love to spend my spare time tinkering and creating. I have built anything from Arduino-based bicycle speedometers and inverse pendulum “Segway” robots to my most recent creation of a desktop CNC vertical mill.</p>
+          </div>
+        </div>
+        <div class="extra content">
+          <a href=“https://www.linkedin.com/in/jeremy-deutch-2286738a/“ target=“_blank”>
+            <i class="linkedin square icon"></i>
+            Profile
+          </a>
         </div>
       </div>
       <div class="card">
+        <div class="image">
+          <img src="img/team/jarred_gregory.jpg">
+        </div>
         <div class="content">
           <div class="header">Jarred Gregory</div>
           <div class="meta">
             Mechanical Engineer Undergrad
           </div>
+          <div class="description">
+            I am from Monument, CO and have lived here since I was 2. I came to Colorado State University as an undeclared major and have since been admitted into the Mechanical Engineering program with a minor in Business Administration. I love hiking, lifting, and going to different countries with friends. I have an interest in learning control solutions and look forward to seeing what this senior design team can do.
+          </div>
+        </div>
+        <div class="extra content">
+          <a href="https://www.linkedin.com/in/jarred-gregory-a7162810a/" target="_blank">
+            <i class="linkedin square icon"></i>
+            Profile
+          </a>
         </div>
       </div>
       <div class="card">
@@ -33,13 +70,29 @@
           <div class="meta">
             Electrical Engineer Undergrad
           </div>
+          <div class="description">
+            I am in my fourth and final year here at CSU studying Electrical engineering. Although I transferred in from Red Rocks community college, I am happy to be in my senior year. I have had two internships at two different businesses. The first was EG power engineering. They were consulting engineers that designed the electrical layouts of residential and commercial buildings. The second was at Burns and McDonnell’s. Here I learned the telecom portion of the electrical substations that we all depend on every day.
+          </div>
+        </div>
+        <div class="extra content">
+          <a href="https://www.linkedin.com/in/indiana-hooser-b82869117/" target="_blank">
+            <i class="linkedin square icon"></i>
+            Profile
+          </a>
         </div>
       </div>
       <div class="card">
+        <div class="image">
+          <img src="img/team/zachary_smith.jpg">
+        </div>
         <div class="content">
           <div class="header">Zachary Smith</div>
           <div class="meta">
             Computer Engineer Undergrad
+          </div>
+          <div class="description">
+            <p>I have been developing, tinkering, and programming since I was 14 years-old. I have a passion for taking on projects that, by nature, cannot reveal the end product design from the get-go. I have been working at CSU's Powerhouse Energy Campus since August 2016 helping to solve important problems. I have picked up a lot of hats since starting there; LabVIEW, C#, C++, and big data engineering have occupied my working hours here.</p>
+            <p>I have a passion for working with and giving of myself to people. In my free time, you may find me with a book, playing guitar, listening to vinyl, or doing all of the above with a close friend. I am excited for this project and all of its challenges.</p>
           </div>
         </div>
         <div class="extra content">
