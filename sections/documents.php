62b08f3de07c1a308ac527caad2b3820952c7908
zacharyasmith <zach@medialusions.com>
2017-12-10 22:23:48 -0700

    Initial document update.

--- sections/documents.php
+++ sections/documents.php
@@ -6,6 +6,7 @@
     </h1>
     <h3 class="ui inverted header">
       To obtain a copy of our team charter, send an email to <a href="mailto:smithza@rams.colostate.edu">smithza@rams.colostate.edu</a>.
+	  First semester presentation available here <a href="presentation.pptx">(.pptx)</a> <a href="presentation.pdf">(.pdf)</a>.
     </h3>
   </div>
 </div>
